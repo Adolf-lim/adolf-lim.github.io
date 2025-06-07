e01a79616e11dcb79fab693890a3f0f10b3868de
biplovkhatri <biplovkhatri36@gmail.com>
2025-06-07 18:29:01 +0900

    contact us

--- contact.php
+++ contact.php
@@ -12,29 +12,26 @@
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
-        
-
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "example@example.com";
+        $recipient = "biplovkhatri36@gmail.com";
 
         // Build the email content.
-        $email_content = "Name $name\n";
-        $email_content .= "Email \n$message\n";
-        $email_content .= "Message \n$message\n";
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n";
+        $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
 
         // Send the email.
-        if (mail($recipient,  $email_content, $email_headers)) {
+        if (mail($recipient, "New Contact Form Submission", $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong ande we couldn't send your message.";
+            echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
