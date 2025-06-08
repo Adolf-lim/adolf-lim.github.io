9f7c0e657c47947391a91ea8afcc6ab92c294a19
biplovkhatri <biplovkhatri36@gmail.com>
2025-06-08 11:23:14 +0900

    added search icon in service

--- contact.php
+++ contact.php
@@ -1,43 +1,33 @@
 <?php
-    /*
-    name
-    email
-    message
-    */
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-        $name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
 
-        // Set the recipient email address.
-        $recipient = "biplovkhatri36@gmail.com";
+    $recipient = "biplovkhatri36@gmail.com";
+    $email_subject = "New Contact Form Submission";
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n";
-        $email_content .= "Message:\n$message\n";
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+    $email_headers = "From: $name <$email>\r\n";
+    $email_headers .= "Reply-To: $email\r\n";
 
-        // Send the email.
-        if (mail($recipient, "New Contact Form Submission", $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-
+    if (mail($recipient, $email_subject, $email_content, $email_headers)) {
+        // Option 1: Redirect to thank-you page
+        header("Location: thank-you.html");
+        exit;
+        
+        // Option 2: Uncomment to show an alert instead
+        // echo "<script>alert('Message sent successfully!'); window.location.href='contact.html';</script>";
     } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Oops! Something went wrong and we couldn't send your message.";
     }
 
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
 ?>
