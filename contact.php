d5d5f319e14b56301c41bf9cd0c8f99d2b494866
biplovkhatri <biplovkhatri36@gmail.com>
2025-06-08 16:16:10 +0900

    mail.php

--- contact.php
+++ contact.php
@@ -1,33 +1,35 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $name = str_replace(["\r","\n"], [" ", " "], $name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    $recipient = "biplovkhatri36@gmail.com";
-    $email_subject = "New Contact Form Submission";
-
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n";
-    $email_content .= "Message:\n$message\n";
-
-    $email_headers = "From: $name <$email>\r\n";
-    $email_headers .= "Reply-To: $email\r\n";
-
-    if (mail($recipient, $email_subject, $email_content, $email_headers)) {
-        // Option 1: Redirect to thank-you page
-        header("Location: thank-you.html");
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo "Please complete the form and use a valid email address.";
         exit;
-        
-        // Option 2: Uncomment to show an alert instead
-        // echo "<script>alert('Message sent successfully!'); window.location.href='contact.html';</script>";
-    } else {
-        echo "Oops! Something went wrong and we couldn't send your message.";
     }
 
+    $recipient = "biplovkhatri36@gmail.com";
+    $subject = "New Contact Form Submission from ratedisco.com";
+
+    $content = "Name: $name\n";
+    $content .= "Email: $email\n";
+    $content .= "Message:\n$message\n";
+
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($recipient, $subject, $content, $headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
 } else {
     http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+    echo "Invalid request.";
 }
 ?>
